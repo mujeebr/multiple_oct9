82b16c8f4702f99a32c608642423ffeaaa724102
mujeebr <mujeebr469@gmail.com>
2024-10-09 11:45:19 +0530

    modified

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import streamlit as st
 import joblib
+import numpy as np
 st.title("House prediction app")
 st.image("naresh_it.jpeg")
 
 House_size=st.number_input("Please enter House Size:")
 Bedrooms=st.number_input('Please enter No of Bedrooms:')
 
-model = joblib.load("mul_lin.pkl")
+model = joblib.load("mul_linear.pkl")
 
 if st.button("Predict"):
-    output= model.predict([[House_size,Bedrooms]])
+    features = np.array([[House_size,Bedrooms]])
+    output= model.predict(features)
     st.write(f"The price of the house is {output[0]}")
 
 
